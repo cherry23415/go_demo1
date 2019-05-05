2e94cccdc63f24d4118a62e186180cab085a7854
cherry <cherry@maizuo.com>
2019-05-05 15:08:57 +0800

    vendor提交iris.v5版本

---
+++ vendor/gopkg.in/kataras/iris.v5/iris/doc.go
@@ -0,0 +1,22 @@
+package main // import "github.com/kataras/iris/iris"
+
+/*
+
+- install iris command line tool
+$ go get -u gopkg.in/kataras/iris.v5
+
+
+- get command
+Downloads the mongo sample project prototype, open this folder to your explorer,
+builds, runs and watches for source code changes (hot-reload).
+
+$ iris get mongo
+
+
+- run command
+Runs and monitors for source code changes(hot-reload).
+
+$ cd mysites/site1
+$ iris run main.go
+
+*/
